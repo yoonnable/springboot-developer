ba3b04c5ff0234eba323d4978194d53b72a84534
yoonnable <yoonnable@gmail.com>
2023-07-22 00:23:47 +0900

    project init

---
+++ src/main/java/me/yoonnable/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -0,0 +1,11 @@
+package me.yoonnable.springbootdeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDeveloperApplication { //이 프로젝트에서 메인 클래스로 사용 할 것!!
+    public static void main(String[] args) { //메인 메소드
+        SpringApplication.run(SpringBootDeveloperApplication.class, args);
+    }
+}
