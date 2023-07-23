c3a174eb0d406dd36315f96c1b3d2d0ab35c2781
yoonnable <yoonnable@gmail.com>
2023-07-24 00:15:11 +0900

    스프링 부트 3 테스트 JUnit 사용 예제

--- src/test/java/JUnitTest.java
+++ src/test/java/JUnitTest.java
@@ -14,15 +14,15 @@ public class JUnitTest {
         //첫 번째 인수 : 기대 값
         //두 번째 인수 : 검증할 값
     }
-    @DisplayName("1 + 3는 4이다") //테스트 이름 명시
-    @Test //테스트를 수행하는 메서드로 만들어줌 -> 다른 테스트에 영향을 주지 않고 독립적
-    public void junitFailedTest() {
-        int a = 1;
-        int b = 3;
-        int sum = 3;
-
-        Assertions.assertEquals(sum, a + b); //두 인수 값이 같은지 확인
-        //첫 번째 인수 : 기대 값
-        //두 번째 인수 : 검증할 값
-    }
+//    @DisplayName("1 + 3는 4이다") //테스트 이름 명시
+//    @Test //테스트를 수행하는 메서드로 만들어줌 -> 다른 테스트에 영향을 주지 않고 독립적
+//    public void junitFailedTest() {
+//        int a = 1;
+//        int b = 3;
+//        int sum = 3;
+//
+//        Assertions.assertEquals(sum, a + b); //두 인수 값이 같은지 확인
+//        //첫 번째 인수 : 기대 값
+//        //두 번째 인수 : 검증할 값
+//    }
 }
---
+++ src/test/java/me/yoonnable/springbootdeveloper/TestControllerTest.java
@@ -0,0 +1,64 @@
+package me.yoonnable.springbootdeveloper;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest // 메인 애플리케이션 클래스에 추가하는 애너테이션인 @SpringBootApplication이 있는 클래스를 찾고 그 클래스에 포함되어있는 빈을 찾은 다음, 테스트용 애플리케이션 컨택스트 생성
+    @AutoConfigureMockMvc // MockMvc(애플리케이션을 서버에 배포하지 않고도 테스트용 MVC 환경을 만들어 요청, 전송, 응답 기능을 제공하는 유틸리티 클래스) 생성 및 자동 구성
+class TestControllerTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+    
+    @Autowired
+    private WebApplicationContext context;
+    
+    @Autowired
+    private MemberRepository memberRepository;
+    
+    @BeforeEach // 태스트 실행 전 실행하는 메서드
+    public void mockMvcSetUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+    
+    @AfterEach // 태스트 실행 후 실행하는 메서드 - member 테이블에 있는 데이터들을 모두 삭제해줌
+    public void cleanUp() {
+        memberRepository.deleteAll();
+    }
+
+    @DisplayName("getAllMembers : 아티클 조회에 성공한다.")
+    @Test
+    public void getAllMembers() throws Exception {
+        // given : 테스트 준비 과정
+        // 멤버를 저장합니다.
+        final String url = "/test";
+        Member saveMember = memberRepository.save(new Member(1L, "홍길동"));
+
+        // when : 실제 테스트 실행
+        // 멤버 리스트를 조회하는 API를 호출합니다.
+        final ResultActions result = mockMvc.perform(get(url).accept(MediaType.APPLICATION_JSON));
+
+        // then : 테스트 결과가 잘 되었는지 검증
+        // 응답 코드가 200 OK이고, 반환받은 값 중에 0번째 요소의 id와 name이 저장된 값과 같은지 확인합니다.
+        result.andExpect((status().isOk()))
+                // 응답의 0번째 값이 DB에 저장한 값과 같은지 확인
+                .andExpect(jsonPath("$[0].id").value(saveMember.getId()))
+                .andExpect(jsonPath("$[0].name").value(saveMember.getName()));
+    }
+
+}
